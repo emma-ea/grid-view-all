cc1eb770037591fab0f4585e5ba622d0662e56b8
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2022-11-15 21:59:13 +0000

    grid params and initial setup
    define input params and initial grid setup

--- lib/grid_view_all.dart
+++ lib/grid_view_all.dart
@@ -1,7 +1,94 @@
 library grid_view_all;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/widgets.dart';
+
+/// Defines a widget which is used to display a grid of items with a view all button
+class GridViewAllWidget extends StatefulWidget {
+  const GridViewAllWidget({
+    Key? key,
+    required this.items,
+    required this.defaultItemsCount,
+    this.animationDuration,
+    this.topPad = 10,
+    this.bottomPad = 10,
+    this.leftPad = 0,
+    this.rightPad = 0,
+    this.itemsPerColumn = 3,
+    this.shrinkWrap = false,
+    this.makeScrollable = false,
+  }) : super(key: key);
+
+  /// Defines the number of items to show as default when the gridview is rendered.
+  /// Default is [_defaultItemsCount]
+  final int? defaultItemsCount;
+
+  /// Defines List of items to be populated by the gridview
+  final List<String>? items;
+
+  /// Defines the animation duration value for scrolling up the list
+  final int? animationDuration;
+
+  /// Defines the top padding for grid items
+  /// Default is 10
+  final double topPad;
+
+  /// Defines the right padding for grid items
+  /// Default is 0
+  final double rightPad;
+
+  /// Defines the bottom padding for grid items
+  /// Default is 10
+  final double bottomPad;
+
+  /// Defines the left padding for grid items
+  /// Default is 0
+  final double leftPad;
+
+  /// Defines the number of item columns for the grid view
+  /// Default is 3
+  final int itemsPerColumn;
+
+  final bool shrinkWrap;
+
+  /// Defines if the grid view is scrollable within its parent
+  /// Default is false
+  final bool makeScrollable;
+
+  @override
+  State<StatefulWidget> createState() {
+    return _GridViewAllWidgetState();
+  }
+}
+
+class _GridViewAllWidgetState extends State<GridViewAllWidget> {
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      shrinkWrap: widget.shrinkWrap,
+      physics: widget.makeScrollable ? const ScrollPhysics() : const NeverScrollableScrollPhysics(),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: widget.itemsPerColumn),
+      itemBuilder: ((context, index) {
+        return _buildGridItem(widget.items![index], index);
+      }),
+      itemCount: widget.items!.length,
+    );
+  }
+
+  Widget _buildGridItem(String item, int index) {
+    return Padding(
+      padding: EdgeInsets.only(
+        top: widget.topPad,
+        bottom: widget.bottomPad,
+        right: widget.rightPad,
+        left: widget.leftPad,
+      ),
+      child: Container(
+        child: Center(
+          child: Text(item),
+        ),
+      ),
+    );
+  }
 }
